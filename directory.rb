16281bc87bfe03a2c6684c2aba5662863e05371d
williamjones <willj836@gmail.com>
2022-10-22 18:00:23 +0100

    used an array

--- directory.rb
+++ directory.rb
@@ -1,22 +1,31 @@
 student_count = 11
-# first we print the list of students
+# let's put all of the students into an array
+students = [
+  "Dr Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
+]
+# then we print them
 puts "The students of Villains Academy"
 puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-# then we print the total number of students
-# puts adds a new line return after printing each argument
-# print does not add a new line
-# if you wanted to add a new line after print you would do it like this:
-# print "line1\nline2\nline3\n"
-# remember, it has to be double quotes as the \n is an escape sequence
+puts students[0]
+puts students[1]
+puts students[2]
+puts students[3]
+puts students[4]
+puts students[5]
+puts students[6]
+puts students[7]
+puts students[8]
+puts students[9]
+puts students[10]
+# print the total
 puts "Overall, we have #{student_count} great students"
