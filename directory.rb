22394b7e8484c51efcfa9885007da41d09cb5a32
williamjones <willj836@gmail.com>
2022-10-22 17:46:14 +0100

    added comments

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# first we print the list of students
 puts "The students of Villains Academy"
 puts "-------------"
 puts "Dr. Hannibal Lecter"
@@ -11,6 +12,12 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+# then we print the total number of students
+# puts adds a new line return after printing each argument
+# print does not add a new line
+# if you wanted to add a new line after print you would do it like this:
+# print "line1\nline2\nline3\n"
+# remember, it has to be double quotes as the \n is an escape sequence
 print "Overall, we have "
 print 11
 puts " great students"
