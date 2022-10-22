81e1492f9119d368fea0cd585a9cdc2dd61ffe72
williamjones <willj836@gmail.com>
2022-10-22 17:48:43 +0100

    used string interpolation and replaced raw num

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+student_count = 11
 # first we print the list of students
 puts "The students of Villains Academy"
 puts "-------------"
@@ -18,6 +19,4 @@ puts "Norman Bates"
 # if you wanted to add a new line after print you would do it like this:
 # print "line1\nline2\nline3\n"
 # remember, it has to be double quotes as the \n is an escape sequence
-print "Overall, we have "
-print 11
-puts " great students"
+puts "Overall, we have #{student_count} great students"
