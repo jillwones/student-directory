d4deb5c0a0353159da039e3fcc2ea10ddc46f031
williamjones <willj836@gmail.com>
2022-10-22 18:04:03 +0100

    replaced lots of puts with a .each

--- directory.rb
+++ directory.rb
@@ -1,4 +1,3 @@
-student_count = 11
 # let's put all of the students into an array
 students = [
   "Dr Hannibal Lecter",
@@ -16,16 +15,6 @@ students = [
 # then we print them
 puts "The students of Villains Academy"
 puts "-------------"
-puts students[0]
-puts students[1]
-puts students[2]
-puts students[3]
-puts students[4]
-puts students[5]
-puts students[6]
-puts students[7]
-puts students[8]
-puts students[9]
-puts students[10]
+students.each { |student| puts student }
 # print the total
-puts "Overall, we have #{student_count} great students"
+puts "Overall, we have #{students.count} great students"
