d4c67b517c8575b4ac257c9379e356a582b7074a
williamjones <willj836@gmail.com>
2022-10-24 16:57:11 +0100

    added a method to save student data to a csv file, also added .gitignore to not track the csv file

--- directory2.rb
+++ directory2.rb
@@ -1,18 +1,3 @@
-# let's put all of the students into an array
-# students = [
-#   {name: "Dr Hannibal Lecter", cohort: :november},
-#   {name: "Darth Vader", cohort: :november},
-#   {name: "Nurse Ratched", cohort: :november},
-#   {name: "Michael Corleone", cohort: :november},
-#   {name: "Alex DeLarge", cohort: :november},
-#   {name: "The Wicked Witch of the West", cohort: :november},
-#   {name: "Terminator", cohort: :november},
-#   {name: "Freddy Krueger", cohort: :november},
-#   {name: "The Joker", cohort: :november},
-#   {name: "Joffrey Baratheon", cohort: :november},
-#   {name: "Norman Bates", cohort: :november}
-# ]
-
 require 'io/console'
 
 @students = []
@@ -20,6 +5,7 @@ require 'io/console'
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end 
 
@@ -33,6 +19,7 @@ def process selection
   case selection 
     when "1" then input_students
     when "2" then show_students 
+    when "3" then save_students
     when "9" then exit
     else 
       puts "Invalid selection"
@@ -115,6 +102,18 @@ def input_students
     puts "Now we have #{@students.count} #{@students.count == 1 ? 'student': 'students'}"
   end
 end   
+
+def save_students
+  # open the folder for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(', ')
+    file.puts csv_line
+  end 
+  file.close 
+end 
 
 # nothing happens until we call the methods
 interactive_menu
